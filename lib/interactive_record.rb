a362318802be823c33262368a33ce2dc7f27004d
Timur Mishiev <timurmishiev@gmail.com>
2020-04-06 03:30:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,7 +21,11 @@ class InteractiveRecord
     column_names.compact
   end
   
-  
+  def initialize(objects={})
+    objects.each do |k, v|
+      self.send("#{k}=", v)
+    end
+  end
   
   
 end
